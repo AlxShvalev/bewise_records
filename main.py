d8c77f002bee80c1d554cecef3fcfe405541ee9b
AlxShvalev <shvalevalx@gmail.com>
2023-05-30 20:24:21 +0500

    starting application

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import uvicorn
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from app.application import create_app
+from app.core.config import settings
 
+app = create_app()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    """Starting application."""
+    uvicorn.run(app, host=settings.ip_address, port=settings.ip_port)
