4952a2a5018db208dc28b7b2b39ace9e7c92a2f5
AlxShvalev <shvalevalx@gmail.com>
2023-06-05 13:23:31 +0500

    added CouldntDecodeException, refactored other exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -3,20 +3,14 @@ from http import HTTPStatus
 from fastapi import HTTPException
 
 
-class AppException(HTTPException):
-
-    status_code: int = None
-    detail: str = ""
-
-
-class NotFoundException(AppException):
+class NotFoundException(HTTPException):
 
     def __init__(self, obj, **params):
         self.status_code = HTTPStatus.NOT_FOUND
         self.detail = f"{obj} with {params} not found."
 
 
-class UnauthorizedException(AppException):
+class UnauthorizedException(HTTPException):
     def __init__(self):
         self.status_code = HTTPStatus.UNAUTHORIZED
         self.detail = "User is not authorized or entered invalid token."
@@ -27,3 +21,9 @@ class AlreadyExistsException(HTTPException):
     def __init__(self, obj, **params):
         self.status_code = HTTPStatus.BAD_REQUEST
         self.detail = f"{obj} with {params} already exists."
+
+
+class CouldntDecodeException(HTTPException):
+    def __init__(self):
+        self.status_code = HTTPStatus.BAD_REQUEST
+        self.detail = "Please send correct WAV file."
